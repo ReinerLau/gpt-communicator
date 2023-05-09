09eb3cecd5fc7f45600e28ee0b0e4a63068328f3
ReinerLau <lk850593913@gmail.com>
2023-05-09 17:13:28 +0800

    feat: 语音

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,14 +10,20 @@
     <div class="flex-1 overflow-auto p-5">
       <template v-for="item in messages" :key="item.content">
         <div v-if="item.role === 'user'" class="flex justify-end items-center mb-4">
-          <div class="max-w-full p-2 rounded bg-slate-600 text-white break-all shadow-md">
+          <div
+            @click="() => handlePlay(item.content)"
+            class="max-w-full p-2 rounded bg-slate-600 text-white break-all shadow-md cursor-pointer"
+          >
             {{ item.content }}
           </div>
           <div class="text-2xl ml-3">🙍‍♂️</div>
         </div>
         <div v-if="item.role === 'assistant'" class="flex justify-start items-center mb-4">
           <div class="text-2xl mr-3">‍️🤖</div>
-          <div class="max-w-full p-2 rounded bg-slate-600 text-white break-all shadow-md">
+          <div
+            @click="() => handlePlay(item.content)"
+            class="max-w-full p-2 rounded bg-slate-600 text-white break-all shadow-md cursor-pointer"
+          >
             {{ item.content }}
           </div>
         </div>
@@ -30,43 +36,32 @@
           v-model="question"
           class="flex-1 bg-[#40414f] outline-none text-white"
         />
-        <img
-          @click="handleRecordStart"
-          class="h-full cursor-pointer"
-          src="@/assets/microphone.svg"
-        />
+        <img @click="handleRecord" class="h-full cursor-pointer" src="@/assets/microphone.svg" />
       </div>
       <div
-        class="bg-amber-200 h-1/2 rounded px-2 flex items-center ml-2 cursor-pointer select-none hover:bg-amber-100 active:bg-yellow-200"
+        class="bg-amber-200 h-1/2 rounded px-2 flex items-center ml-2 cursor-pointer select-none hover:bg-amber-100 active:bg-amber-200"
         @click="sendMessage"
       >
         SEND
       </div>
-    </div>
-    <!-- <div>
-      <select @change="handleVoiceChange" ref="voiceOptions" class="w-full" id="voiceOptions">
-        <option v-for="voice in voices" :key="voice.name" :value="voice.name">
-          {{ voice.name }}
-        </option>
-      </select>
-    </div> -->
-    <!-- <div class="flex justify-end">
-      <div class="p-2 rounded bg-green-500 text-white break-all shadow-md">
-        {{ question }}
+      <div
+        class="bg-red-500 text-white h-1/2 rounded px-2 flex items-center ml-2 cursor-pointer select-none hover:bg-red-400 active:bg-red-500"
+        @click="clearMessage"
+      >
+        CLEAR
       </div>
     </div>
-    <div class="flex justify-start">
-      <div @touchstart="handlePlay" class="max-w-xs p-2 rounded bg-white break-all shadow-md">
-        <Markdown :source="answer"></Markdown>
-      </div>
-    </div> -->
   </div>
 </template>
 
 <script setup lang="ts">
-import { ChatCompletionRequestMessageRoleEnum, type ChatCompletionRequestMessage } from 'openai'
-import { Configuration, OpenAIApi } from 'openai'
-import { nextTick, ref } from 'vue'
+import {
+  ChatCompletionRequestMessageRoleEnum,
+  Configuration,
+  OpenAIApi,
+  type ChatCompletionRequestMessage
+} from 'openai'
+import { ref } from 'vue'
 
 const synth = window.speechSynthesis
 const SpeechRecognition =
@@ -74,13 +69,8 @@ const SpeechRecognition =
 const recognition = new SpeechRecognition()
 let voice
 const question = ref('')
-const answer = ref('')
 const recordStatus = ref(false)
 const voices = ref<any>([])
-
-function handleVoiceChange(e) {
-  voice = voices.value[e.target.selectedIndex]
-}
 
 function initSpeech() {
   voices.value = synth.getVoices()
@@ -91,29 +81,33 @@ function initSpeech() {
   }
 }
 
-function handlePlay() {
-  if (synth.speaking) {
-    synth.cancel()
-    return
-  }
-  const utter = new SpeechSynthesisUtterance(answer.value)
+function handlePlay(message) {
+  if (synth.speaking) synth.cancel()
+  const utter = new SpeechSynthesisUtterance(message)
   utter.voice = voice
   synth.speak(utter)
 }
 
 function initRecognition() {
   recognition.continuous = true
-  // recognition.interimResults = true
+  recognition.interimResults = true
   recognition.maxAlternatives = 1
   recognition.onresult = (e) => {
     const text = e.results[0][0].transcript
     question.value = text
-    recordStatus.value = false
+  }
+}
+
+function handleRecord() {
+  if (recordStatus.value) {
+    handleRecordEnd()
+  } else {
+    handleRecordStart()
   }
 }
 
 function handleRecordStart() {
-  // synth.cancel()
+  synth.cancel()
   recordStatus.value = true
   recognition.start()
 }
@@ -121,13 +115,6 @@ function handleRecordStart() {
 function handleRecordEnd() {
   recordStatus.value = false
   recognition.stop()
-  if (!question.value) return
-  messages.value.push({
-    role: 'user',
-    content: question.value
-  })
-  question.value = ''
-  // getAnswer()
 }
 
 const messages = ref<ChatCompletionRequestMessage[]>([])
@@ -149,6 +136,7 @@ async function getAnswer() {
       messages: messages.value.filter((item) => item.content !== 'Thinking...')
     })
     messages.value.slice(-1)[0].content = completion.data.choices[0].message?.content as string
+    handlePlay(messages.value.slice(-1)[0].content)
   } catch (error: any) {
     answer.content = error.message
   }
@@ -156,12 +144,17 @@ async function getAnswer() {
 
 function sendMessage() {
   if (!question.value) return
+  recordStatus.value = false
   messages.value.push({
     role: ChatCompletionRequestMessageRoleEnum.User,
     content: question.value
   })
   question.value = ''
   getAnswer()
+}
+
+function clearMessage() {
+  messages.value = []
 }
 
 initSpeech()
